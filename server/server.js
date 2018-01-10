d81b5f9078d2ccd84d55022ebdd90d4fde3769f0
Computer Science <computerscience@nicolass-air-3.home>
2018-01-10 16:11:33 +0000

    Added geolocation button in index.html and functionality in index.js

--- server/server.js
+++ server/server.js
@@ -25,13 +25,11 @@ io.on('connection', (socket)=>{
     console.log('createMessage',message);
     io.emit(`newMessage`,generateMessage(message.from, message.text));
     callback('this is from the server');
-// socket.broadcast.emit('newMessage',{
-//   from: message.from,
-//   text: message.text,
-//   createdAt: new Date().getTime()
-// })
-
   })
+
+  socket.on('createLocationMessage', (coords)=>{
+    io.emit('newMessage', generateMessage('Admin',`${coords.latitude}, ${coords.longitude}`))
+  });
 
 
   socket.on ('disconnect',()=>{
