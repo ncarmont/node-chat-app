cd509ba69bd56288582bbbdde725d08737fce863
Computer Science <computerscience@nicolass-air-3.home>
2018-01-10 17:29:59 +0000

    Format timestamps using moment.js

---
+++ playground/time.js
@@ -0,0 +1,9 @@
+const moment = require('moment');
+
+
+var someTimeStamp = moment().valueOf();
+console.log(someTimeStamp);
+
+var createdAt = 1234;
+var date = moment(createdAt);
+console.log(date.format('h:mm a'));
